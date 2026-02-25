aa50b20f7e4c3890cc89c84e88f81a36a5ec819c
ANU <Satcorpvk@outlook.com>
2026-02-25 17:56:07 -0600

    Update ThreatMonitor.tsx

--- components/dashboard/ThreatMonitor.tsx
+++ components/dashboard/ThreatMonitor.tsx
@@ -49,7 +49,7 @@ export const ThreatMonitor = () => {
               endAngle={-270}
             >
               <PolarAngleAxis type="number" domain={[0, 100]} angleAxisId={0} tick={false} />
-              <RadialBar minAngle={15} background clockWise dataKey="value" />
+              <RadialBar background clockWise dataKey="value" minPointSize={5} />
               <text x="50%" y="50%" textAnchor="middle" dominantBaseline="middle" className="text-2xl fill-current">
                 {threats.risk}%
               </text>
